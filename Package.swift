89fde1f3f4b9dfd884f6e0215c3852a03129d204
Matthew <matt@kitserve.org.uk>
2018-03-11 16:30:46 +0000

    Declare library product in Package.swift

--- Package.swift
+++ Package.swift
@@ -3,13 +3,14 @@
 import PackageDescription
 
 let package = Package(
-    name: "RxSwiftMidi",
-    dependencies: [
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", "4.0.0"..<"5.0.0")
-    ],
-    targets: [
-        .target(
-            name: "RxSwiftMidi",
-            dependencies: ["RxSwift"])
-    ]
+        name: "RxSwiftMidi",
+        products: [
+            .library(name: "RxSwiftMidi", targets: ["RxSwiftMidi"])
+        ],
+        dependencies: [
+            .package(url: "https://github.com/ReactiveX/RxSwift.git", "4.0.0"..<"5.0.0")
+        ],
+        targets: [
+            .target(name: "RxSwiftMidi", dependencies: ["RxSwift"])
+        ]
 )
